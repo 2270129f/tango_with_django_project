2ca2de6d352a123b9c74aa204197ee9b29826a44
Bita Fardroo <2270129f@student.gla.ac.uk>
2018-02-09 17:31:39 +0100

    end of chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,14 +1,18 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page
+from rango.models import Category
+from rango.models import UserProfile
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-    help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128,help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    # An inline class to provide additional information on the form.
     class Meta:
+        # Provide an association between the ModelForm and a model
         model = Category
         fields = ('name',)
 
@@ -24,8 +28,24 @@ class PageForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
-
+        
         if url and not url.startswith('http://') and not url.startswith('https://'):
             url = 'http://' + url
             cleaned_data['url'] = url
-            return cleaned_data
+            return cleaned_data
+# or specify the fields to include (i.e. not include the category field)
+#fields = ('title', 'url', 'views')
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
+
